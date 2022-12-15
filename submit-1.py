0d4a5b3cd0a77c8654282586d6812f1edeb77bc4
sinsoill <mike.sinsoillier@epfl.ch>
2022-12-15 17:54:11 +0100

    on my way to 1.1

--- submit-1.py
+++ submit-1.py
@@ -13,8 +13,6 @@ def gdb_cmd_exec(p,cmd):
     p.stdin.flush()
 
 def reset():
-    os.system("kill -9 $(lsof -t -i:3500)")
-    os.system("kill -9 $(lsof -t -i:4450)")
     os.system("kill -9 $(lsof -t -i:5111)")
 
 
@@ -24,36 +22,27 @@ def init_gdb() -> Popen:
     os.system(f"cd {PATH}  && {PATH}run_peripheral.sh ")
 
     sleep(2)
-    string_parser = Popen(['gdb', '-q', '-x', f'{PATH}sp_server.py'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    string_parser = Popen(['gdb', 'python3'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
     sleep(1)
     gdb_cmd_exec(string_parser,'set pagination off\n')
     gdb_cmd_exec(string_parser,"set follow-fork-mode child\n")
     gdb_cmd_exec(string_parser,"set breakpoint pending on\n")
-    sleep(2)
     return string_parser
 
 def task1():
     SP = init_gdb()
     gdb_cmd_exec(SP, "break gcm_crypt_and_tag\n")
-
-    RP = Popen([f"{node_prefix}node", "--no-warnings", f"{path}remote_party"])
-
-def main():
-    try:
-        task1()
-        reset()
-    except:
-        pass
-
+    gdb_cmd_exec(SP, f"run {PATH}/sp_server.py\n")
+    os.system("./start.sh")
+    sleep(1)
+    gdb_cmd_exec(SP, "continue\n")
+    sleep(1)
+    gdb_cmd_exec(SP, "continue\n")
+    sleep(1)
+    gdb_cmd_exec(SP, COMMAND+"\n")
+    
 if __name__ == "__main__":
     reset()
-    try:
-        main()
-    except Exception as e:
-        print(e)
-        print(f"An error occured in main!")
-    finally:
-        reset()
-    os.system("cd /home/isl/t1 && /home/isl/t1/run.sh")
-    print("Exploit done")
-    exit()
+    task1() 
+
+
